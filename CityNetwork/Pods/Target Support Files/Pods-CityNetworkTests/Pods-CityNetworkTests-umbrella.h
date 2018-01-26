5d1045d4f6cb4dbdcbe4e6464ff6d87c1bf3cbfd
anhnguyentrung <nguyentrunganh.bk@gmail.com>
2018-01-26 15:00:14 +0900

    - change project name

--- CityNetwork/Pods/Target Support Files/Pods-CityNetworkTests/Pods-CityNetworkTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_CityNetworkTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CityNetworkTestsVersionString[];
-
--- CityNetwork/Pods/Target Support Files/Pods-CityNetworkUITests/Pods-CityNetworkUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CityNetworkUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CityNetworkUITests
-@end
